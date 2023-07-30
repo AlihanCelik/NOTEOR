9b0c5b312c17943350eb8c9bef55ca2cd26ecf5c
Alihan <alihan_08032003@hotmail.com>
2023-07-30 15:23:51 +0300

    NotesAPP

--- app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
@@ -2,20 +2,57 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notesapp.database.NotesDatabase
+import com.example.notesapp.entities.Notes
 import kotlinx.android.synthetic.main.activity_create_note.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlinx.coroutines.*
 
 class CreateNoteActivity : AppCompatActivity() {
+    var currentDate:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         val sdf =SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-        val currentDate=sdf.format(Date())
+        currentDate=sdf.format(Date())
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_note)
         tvDateTime.text=currentDate
 
         backButton.setOnClickListener {
             finish()
+        }
+        saveButton.setOnClickListener {
+            saveNote()
+        }
+
+    }
+    private fun saveNote(){
+        if(notes_title.text.toString().isNullOrEmpty()){
+            Toast.makeText(this,"Note Title is Required", Toast.LENGTH_SHORT).show()
+        }
+        else if(notes_sub_title.text.toString().isNullOrEmpty()){
+            Toast.makeText(this,"Note Sub Title is Required",Toast.LENGTH_SHORT).show()
+
+        }
+        else if(notes_desc.text.toString().isNullOrEmpty()){
+            Toast.makeText(this,"Note Description is Required",Toast.LENGTH_SHORT).show()
+
+        }
+        else{
+            GlobalScope.launch{
+                var notes = Notes()
+                notes.title=notes_title.text.toString()
+                notes.subTitle=notes_sub_title.text.toString()
+                notes.noteText=notes_desc.text.toString()
+                notes.dateTime=currentDate
+                applicationContext?.let {
+                    NotesDatabase.getDatabase(it).noteDao().insertNotes(notes)
+                    notes_title.setText("")
+                    notes_sub_title.setText("")
+                    notes_desc.setText("")
+                }
+            }
         }
 
     }

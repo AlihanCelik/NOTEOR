1f56f8b3387a0f7843f092a06acc25e985bf4e91
Alihan <alihan_08032003@hotmail.com>
2023-07-30 13:22:10 +0300

    NotesAPP

--- app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
@@ -3,13 +3,20 @@ package com.example.notesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_create_note.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CreateNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sdf =SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate=sdf.format(Date())
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_note)
+        tvDateTime.text=currentDate
+
         backButton.setOnClickListener {
             finish()
         }
+
     }
 }

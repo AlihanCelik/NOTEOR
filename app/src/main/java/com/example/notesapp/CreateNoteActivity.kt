78e44bbc88b546b365e4feca625209cd5d0fa923
Alihan <alihan_08032003@hotmail.com>
2023-08-03 16:56:52 +0300

    NotesAPP

--- app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
@@ -1,12 +1,16 @@
 package com.example.notesapp
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.example.notesapp.database.NotesDatabase
 import com.example.notesapp.entities.Notes
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -14,11 +18,23 @@ import kotlinx.android.synthetic.main.activity_create_note.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlinx.coroutines.*
+import java.io.File
+import android.Manifest
+import android.content.Context
+import android.net.Uri
+import java.io.FileOutputStream
+import java.io.IOException
 
 class CreateNoteActivity : AppCompatActivity() {
     var currentDate:String? = null
-
+    private var getFile: File? = null
     var color="blue"
+    companion object {
+        private val READ_STORAGE_PERMISSION = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        private val WRITE_STORAGE_PERMISSION = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        const val REQUEST_CODE_READ_STORAGE_PERMISSION = 10
+        const val REQUEST_CODE_WRITE_STORAGE_PERMISSION = 20
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         val moonBlue = resources.getColor(R.color.moonBlue)
         val moonPink = resources.getColor(R.color.moonPink)
@@ -40,6 +56,10 @@ class CreateNoteActivity : AppCompatActivity() {
         }
         saveButton.setOnClickListener {
             saveNote()
+        }
+        btn_clear_added_img.setOnClickListener {
+            getFile = null
+            layout_img_preview.visibility = View.GONE
         }
         more.setOnClickListener {
             val bottomSheet =
@@ -78,11 +98,68 @@ class CreateNoteActivity : AppCompatActivity() {
                 colorView.setBackgroundColor(moonRed)
 
             }
+            bottomSheetView.findViewById<View>(R.id.image).setOnClickListener {
+                startGallery()
+
+            }
 
             bottomSheet.setContentView(bottomSheetView)
             bottomSheet.show()
         }
 
+    }
+    fun uriToFile(uri: Uri, context: Context): File? {
+        val file = File(context.cacheDir, "temp_file")
+        try {
+            val inputStream = context.contentResolver.openInputStream(uri)
+            val outputStream = FileOutputStream(file)
+            inputStream?.copyTo(outputStream)
+            inputStream?.close()
+            outputStream.close()
+            return file
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val permReqLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val granted = permissions.entries.all {
+                it.value
+            }
+            if (granted) {
+                startGallery()
+            }
+        }
+
+    private fun readExternalStorageGranted() = READ_STORAGE_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun writeExternalStorageGranted() = WRITE_STORAGE_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data
+            val inputStream = this.contentResolver.openInputStream(selectedImg!!)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            val myFile = uriToFile(selectedImg, this)
+            getFile = myFile
+            layout_img_preview.visibility = View.VISIBLE
+            img_preview.setImageBitmap(bitmap)
+        }
     }
     private fun saveNote(){
         if(notes_title.text.toString().isNullOrEmpty()){
@@ -105,6 +182,7 @@ class CreateNoteActivity : AppCompatActivity() {
                 notes.noteText=notes_desc.text.toString()
                 notes.dateTime=currentDate
                 notes.color=color
+                notes.imgPath=getFile.toString()
                 applicationContext?.let {
                     NotesDatabase.getDatabase(it).noteDao().insertNotes(notes)
                     notes_title.setText("")

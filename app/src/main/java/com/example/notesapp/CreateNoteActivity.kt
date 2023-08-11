501a4e2c7e5ff21cbaad4591f0c547d3716ae38e
Alihan <alihan_08032003@hotmail.com>
2023-08-11 22:56:57 +0300

    notesApp

--- app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
@@ -46,6 +46,8 @@ class CreateNoteActivity : AppCompatActivity() {
         val moonGreen = resources.getColor(R.color.moonGreen)
         val moonRed=resources.getColor(R.color.moonRed)
         val moonYellow=resources.getColor(R.color.moonYellow)
+        val moonOrange=resources.getColor(R.color.moonOrange)
+        val moonGrey=resources.getColor(R.color.moonGrey)
         val sdf =SimpleDateFormat("dd/M/yyyy hh:mm:ss")
 
         val backgroundYellow=resources.getColor(R.color.background_yellow)
@@ -54,6 +56,8 @@ class CreateNoteActivity : AppCompatActivity() {
         val backgroundGreen=resources.getColor(R.color.background_green)
         val backgroundPink=resources.getColor(R.color.background_pink)
         val backgroundPurple=resources.getColor(R.color.background_purple)
+        val backgroundOrange=resources.getColor(R.color.background_orange)
+        val backgroundGrey=resources.getColor(R.color.background_grey)
         currentDate=sdf.format(Date())
 
         super.onCreate(savedInstanceState)
@@ -134,6 +138,18 @@ class CreateNoteActivity : AppCompatActivity() {
                 color="red"
                 colorView.setBackgroundColor(moonRed)
                 createNote.setBackgroundColor(backgroundRed)
+
+            }
+            bottomSheetView.findViewById<View>(R.id.orange).setOnClickListener {
+                color="orange"
+                colorView.setBackgroundColor(moonOrange)
+                createNote.setBackgroundColor(backgroundOrange)
+
+            }
+            bottomSheetView.findViewById<View>(R.id.grey).setOnClickListener {
+                color="grey"
+                colorView.setBackgroundColor(moonGrey)
+                createNote.setBackgroundColor(backgroundGrey)
 
             }
             bottomSheetView.findViewById<View>(R.id.image).setOnClickListener {

495263037d6e323fa09ccf026eda738cd3518467
Alihan <alihan_08032003@hotmail.com>
2023-08-19 14:57:58 +0300

    notesApp

--- app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
@@ -110,6 +110,7 @@ class CreateNoteActivity : AppCompatActivity() {
         }
 
         more.setOnClickListener {
+
             val bottomSheet =
                 BottomSheetDialog(this@CreateNoteActivity, R.style.BottomSheetDialogTheme)
             val bottomSheetView = LayoutInflater.from(applicationContext).inflate(

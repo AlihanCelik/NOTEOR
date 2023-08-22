f5ca8e01b0badb1784c3d0532e667ab95bd6d5f7
Alihan <alihan_08032003@hotmail.com>
2023-08-22 15:42:41 +0300

    notesApp

--- app/src/main/java/com/example/notesapp/FavoritesActivtity.kt
+++ app/src/main/java/com/example/notesapp/FavoritesActivtity.kt
@@ -2,15 +2,70 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_create_note.*
+import android.widget.SearchView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notesapp.Adapter.NotesAdapter
+import com.example.notesapp.database.NotesDatabase
+import com.example.notesapp.entities.Notes
+import kotlinx.android.synthetic.main.activity_favorites_activtity.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
 
 class FavoritesActivtity : AppCompatActivity() {
+    var arrNotes = ArrayList<Notes>()
+    var notesAdapter: NotesAdapter = NotesAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites_activtity)
         backButton.setOnClickListener {
             finish()
         }
+        recycler_view_fav.setHasFixedSize(true)
+        recycler_view_fav.layoutManager= StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        GlobalScope.launch(Dispatchers.Main){
+            let {
+                var notes = NotesDatabase.getDatabase(this@FavoritesActivtity).noteDao().getAllNotes()
+                notesAdapter!!.setData(notes)
+                arrNotes = notes as ArrayList<Notes>
+                var FavArr = ArrayList<Notes>()
+                for (arr in arrNotes){
+                    if(arr.favorite==true){
+                        FavArr.add(arr)
+                    }
+                }
+                notesAdapter.setData(FavArr)
+                notesAdapter.notifyDataSetChanged()
+                recycler_view_fav.adapter = notesAdapter
+            }
+
+        }
+        searchFavorites.setOnQueryTextListener( object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+
+                var tempArr = ArrayList<Notes>()
+
+                for (arr in arrNotes){
+                    if(arr.favorite==true){
+                        if (arr.title!!.toLowerCase(Locale.getDefault()).contains(p0.toString())){
+                            tempArr.add(arr)
+                        }
+                    }
+
+                }
+
+                notesAdapter.setData(tempArr)
+                notesAdapter.notifyDataSetChanged()
+                return true
+            }
+
+        })
+
     }
 }

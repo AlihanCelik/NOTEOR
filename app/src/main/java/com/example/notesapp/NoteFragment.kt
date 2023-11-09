25133626ed88d35d879fb46810b7f7c1556be91f
Alihan <alihan_08032003@hotmail.com>
2023-11-09 22:59:23 +0300

    notesApp

--- app/src/main/java/com/example/notesapp/NoteFragment.kt
+++ app/src/main/java/com/example/notesapp/NoteFragment.kt
@@ -196,7 +196,6 @@ class NoteFragment : Fragment(), sortCategoryAdapter.SortCategoryClickListener{
             }
         }
     }
-
     override fun onSortCategoryClick(category: Category) {
         updateSortCategory(category.id_category!!)
         loadNotesByCategoryId()

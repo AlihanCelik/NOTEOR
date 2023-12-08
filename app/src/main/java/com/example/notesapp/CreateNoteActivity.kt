7999154a387dc6e52593361aa3bf8d197021fd15
Alihan <alihan_08032003@hotmail.com>
2023-12-09 02:29:03 +0300

    notesApp

--- app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/CreateNoteActivity.kt
@@ -688,7 +688,7 @@ class CreateNoteActivity : AppCompatActivity(),CategoryAdapter.CategoryClickList
             }
             bottomSheetView.findViewById<View>(R.id.image).setOnClickListener {
                 if (hasPermissions()) {
-                    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
                     intent.addCategory(Intent.CATEGORY_OPENABLE)
                     intent.type = "image/*"
                     intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
